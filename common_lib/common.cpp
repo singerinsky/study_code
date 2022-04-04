933af0d92c2541d1f53221b7cb5c6ecba8ab295f
guanlei <singerinsky@gmail.com>
2022-04-04 17:31:15 +0800

    test tupe size

--- common_lib/common.cpp
+++ common_lib/common.cpp
@@ -4,17 +4,19 @@ extern "C" {
 #include <lua.h>
 #include <lualib.h>
 
-static int my_add(lua_State *L) {
-  ::sleep(20);
-  int i = 100;
-  int j = 99;
-  lua_pushnumber(L, i - j);
-  return i - j;
+static int my_add(lua_State* L)
+{
+    ::sleep(20);
+    int i = 100;
+    int j = 99;
+    lua_pushnumber(L, i - j);
+    return i - j;
 }
 
-int luaopen_libluatestlib(lua_State *L) {
-  luaL_Reg luaLoadFun[] = {{"my_add", my_add}, {NULL, NULL}};
-  luaL_newlib(L, luaLoadFun);
-  return 1;
+int luaopen_libluatestlib(lua_State* L)
+{
+    luaL_Reg luaLoadFun[] = { { "my_add", my_add }, { NULL, NULL } };
+    luaL_newlib(L, luaLoadFun);
+    return 1;
 }
 }
