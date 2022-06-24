89f7a416522edbc4870258404e00bb4bcf2fda8c
guanlei <singerinsky@gmail.com>
2022-06-24 08:20:11 +0800

    add python shell

--- src/luafile/single_lua_chain.lua
+++ src/luafile/single_lua_chain.lua
@@ -1,5 +1,9 @@
-
-local g_veczone = {
+local EConfig = {
+    ERowCount = 20,
+    EColumnCount = 20,
+}
+--[[-example
+local g_veczone_back = {
     {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
     {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
     {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
@@ -16,10 +20,23 @@ local g_veczone = {
     {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
     {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
 }
+-]]--
+local g_veczone = {}
+for cindex=1,EConfig.ERowCount do 
+    g_veczone[cindex] = {}
+    for rindex=1,EConfig.ERowCount do 
+        g_veczone[cindex][rindex] = 1
+    end
+end
 
 function print_zone(vecZone)
+    local row_count = #vecZone[1]
+    local row_index = 1
     for _,value in pairs(vecZone) do
-        print(require("serpent").line(value))
+        local str = string.format("%02d",row_index)
+        print(str.." " ..require("serpent").line(value))
+
+        row_index = row_index+1
     end
 end
 
@@ -27,17 +44,29 @@ local g_chain_flag = {}
 
 local g_calc_count = 0
 
+--设置
 function set_zone_value(r,c,zone,value)
-    g_calc_count = g_calc_count + 1
+    --g_calc_count = g_calc_count + 1
     if zone[r] ~= nil and zone[r][c] ~= nil then
         --如果该格子在前期已经被引爆，则不在加入判断,导致图形不对称也是正常的
         if zone[r][c] == 0 then
             return false 
         end
-       zone[r][c] = 0 
+       zone[r][c] = zone[r][c] - 1
        return true
     end
     return false
+end
+
+function is_zone_flag_set(r,c,zone)
+  g_calc_count = g_calc_count + 1
+  if zone[r] ~= nil and zone[r][c] ~= nil then
+      if zone[r][c] == 0 then
+          return {true,false}
+      end
+      return {true,true}
+  end
+  return {false,false}
 end
 
 --左右下
@@ -51,36 +80,63 @@ function effect_node_around(oSingleNode,vecZone)
         local effect_r_index = value[1] + oSingleNode[1]
         local effect_c_index = value[2] + oSingleNode[2]
 
-        --if g_chain_flag[effect_r_index*20 + effect_c_index] == nil then
-            if set_zone_value(effect_r_index, effect_c_index, vecZone, 0) then
-                table.insert(effect_node, { effect_r_index, effect_c_index })
-            end
-        --end
-
-        --TODO check valid
+        local rst = is_zone_flag_set(effect_r_index,effect_c_index,vecZone)
+        --if set_zone_value(effect_r_index, effect_c_index, vecZone, 0) then
+        if rst[1]== true and rst[2] == true then
+            table.insert(effect_node, { effect_r_index, effect_c_index })
+        end
     end
     return effect_node
 end
 
 function do_chain(vecNode, vecZone, dwChainCount)
-    if dwChainCount == 3 then
-        return
-    end
     dwChainCount = dwChainCount + 1
     for _, value in pairs(vecNode) do
+        --do custom logic,击碎冰块
         set_zone_value(value[1], value[2],vecZone, 0)
         g_chain_flag[value[1]*20 + value[2]] = 1
-        local effect_nodes = effect_node_around(value, vecZone)
-        if #effect_nodes ~= 0 then
-            do_chain(effect_nodes, vecZone, dwChainCount)
+        -- 如果已经是第三次，不在连锁反应,只处理到这个节点
+        if dwChainCount <= 3 then 
+            local effect_nodes = effect_node_around(value, vecZone)
+            if #effect_nodes ~= 0 then
+                do_chain(effect_nodes, vecZone, dwChainCount)
+            end
         end
     end
 end
-local first_node = {{5,10}}--,{5,11},{4,10},{4,11}}
-do_chain(first_node,g_veczone,0)
+
+function dump_chain_flag(flags)
+    local celleffect = {}
+    for key,_ in pairs(g_chain_flag) do
+        local r = math.floor(key/20)
+        local c = key%20
+        table.insert(celleffect,{r,c})
+    end
+    print_zone(celleffect)
+end
+
+local first_node = {{5,1}}--,{5,11},{4,10},{4,11}}
 local second_node = {{1,10}}
+g_chain_flag = {}
+g_calc_count = 0
+do_chain(first_node,g_veczone,1)
 print_zone(g_veczone)
 print("calc...."..g_calc_count)
+dump_chain_flag(g_chain_flag)
+g_chain_flag = {}
+g_calc_count = 0
 do_chain(second_node,g_veczone,0)
 print_zone(g_veczone)
 print("calc...."..g_calc_count)
+dump_chain_flag(g_chain_flag)
+
+
+local EIceChainDirConfig = {
+    EIce_ChainDir_L = 1,--左边
+    EIce_ChainDir_D = 2,--下方
+    EIce_ChainDir_R = 3,--右方
+}
+
+for i =EIceChainDirConfig.EIce_ChainDir_L,EIceChainDirConfig.EIce_ChainDir_R do
+    print(i)
+end
--- tools/csv_test.py
+++ tools/csv_test.py
@@ -1,20 +1,25 @@
 import csv
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 
 def read_test():
-    with open("data.csv",mode="r") as f:
+    with open("data.csv", mode="r") as f:
         reader = csv.reader(f)
         header = next(reader)
         for row in reader:
-            print("{}{}:{}={}".format(header[0],row[0],header[1],row[1]))
+            print("{}{}:{}={}".format(header[0], row[0], header[1], row[1]))
 
 
 def write_test():
-    header_list = ["ID","CellValue"]
-    data_list = [[1,"1_1"],[2,"2_2"],[3,"3_3"]]
-    with open("wdata.csv",mode="w") as f:
+    header_list = ["ID", "CellValue"]
+    data_list = [[1, "1_1"], [2, "2_2"], [3, "3_3"]]
+    with open("wdata.csv", mode="w") as f:
         writer = csv.writer(f)
         writer.writerow(header_list)
         writer.writerows(data_list)
+
 
 read_test()
 write_test()
