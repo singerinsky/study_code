edd7064e4133e0dec09bd88c8a87ba40c41bb6bf
guanlei <singerinsky@gmail.com>
2023-02-23 14:28:18 +0800

    ssss

--- src/study_cmake_build_info.h
+++ src/study_cmake_build_info.h
@@ -6,8 +6,8 @@
 struct StudyCmakeBuildInfo
 {
     static inline const std::string GitBranch           = "dev";
-    static inline const std::string CommitSHA           = "2ef6c910454f1cd7a11aa4d8af7f64fe2ed68b18";
-    static inline const std::string Timestamp           = "2023-02-21T18:44:10";
+    static inline const std::string CommitSHA           = "e3e574a6cca5193406797d478706ae098943a469";
+    static inline const std::string Timestamp           = "2023-02-23T14:21:38";
     static inline const std::string StudyCmakeVersion   = "";
 };
 
