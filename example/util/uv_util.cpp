af78a4b4de830898d9cc1be37f0832aac10dca55
guanlei <singerinsky@gmail.com>
2023-04-07 17:55:32 +0800

    fix bug

---
+++ example/util/uv_util.cpp
@@ -0,0 +1,15 @@
+#include "uv_util.h"
+#include <atomic>
+
+static std::atomic_int32_t g_step_incr = 1;
+uint32_t GenerateUniqueID() {
+  // Get current time as milliseconds since Unix epoch
+  auto now = std::chrono::duration_cast<std::chrono::milliseconds>(
+                 std::chrono::system_clock::now().time_since_epoch())
+                 .count();
+
+  // Combine time
+
+  uint32_t id = now << 16 | (g_step_incr.fetch_add(1));
+  return id;
+}
---
+++ example/util/uv_util.h
@@ -0,0 +1,8 @@
+#ifndef BCD8307F_4A5A_4F05_BF1A_CF5FEBD23DD2
+#define BCD8307F_4A5A_4F05_BF1A_CF5FEBD23DD2
+#include <chrono>
+#include <iostream>
+
+//
+uint32_t GenerateUniqueID();
+#endif /* BCD8307F_4A5A_4F05_BF1A_CF5FEBD23DD2 */
