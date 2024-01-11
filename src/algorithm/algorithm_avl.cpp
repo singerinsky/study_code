b38b0f2a741ca6ce86830c1a13db2d667df4acf2
singerinsky <singerinsky@gmail.com>
2024-01-11 08:35:13 +0800

    AVL

--- src/algorithm/algorithm_avl.cpp
+++ src/algorithm/algorithm_avl.cpp
@@ -1,25 +1,70 @@
 #include "algorithm_avl.h"
 #include <gtest/gtest.h>
+/*
+这段代码是一个 C++的单元测试程序，用于测试 AVL
+树的一些基本操作，包括插入、查找和删除等。以下是对这段代码的逐行解释：
 
+#include "algorithm_avl.h"：这是一个包含 AVL 树实现的头文件。
+
+#include <gtest/gtest.h>：这是一个包含 Google Test 框架的头文件。
+
+TEST(AvlTreeTest, TestHeight) {...}：这是一个测试用例，用于测试 AVL
+树的高度计算函数。这个测试用例中，创建了一个空树，然后测试只有一个节点的树的高度，以及有两个节点的树的高度，最后测试有三个节点的树的高度。
+
+TEST(TreeNodeTest, TestPreorder) {...}：这是一个测试用例，用于测试 TreeNode 类的
+preorder()方法。这个测试用例中，创建了一个 TreeNode 对象，然后调用
+preorder()方法并检查返回值。
+
+TEST(TreeNodeTest, TestMiddleorder) {...}：这是一个测试用例，用于测试 TreeNode
+类的 middleorder()方法。这个测试用例中，创建了一个 TreeNode 对象，然后调用
+middleorder()方法并检查返回值。
+
+TEST(TreeNodeTest, TestPostorder) {...}：这是一个测试用例，用于测试 TreeNode
+类的 postorder()方法。这个测试用例中，创建了一个 TreeNode 对象，然后调用
+postorder()方法并检查返回值。
+
+TEST(AvlTreeTest, TestAvlfind) {...}：这是一个测试用例，用于测试 AVL 树的
+avlfind()方法。这个测试用例中，创建了一个 AVL
+树，然后测试值未找到的情况，以及值找到的情况。
+
+TEST(AvlTreeTest, TestInsertAvl) {...}：这是一个测试用例，用于测试 AVL 树的
+insertAvl()方法。这个测试用例中，创建了一个 AVL
+树，然后测试树为空的情况，以及树不为空的情况。
+
+class AVLTreeTest : public testing::Test {...}：这是一个测试类，继承自 Google
+Test 的 Test 类。在这个类中，定义了一个 AVL 树，并提供了一些测试方法。
+
+void SetUp() override {...}：这是 Test
+类的一个虚函数，在每个测试用例运行前会被调用。在这个函数中，创建了一个 AVL 树。
+
+void TearDown() override {...}：这是 Test
+类的一个虚函数，在每个测试用例运行后会被调用。在这个函数中，删除了创建的 AVL
+树。
+
+TEST(AVLTreeTest, FunctionTest)
+{...}：这是一个测试用例，用于测试给定的函数是否按预期工作。这个测试用例中，创建了一个
+AVL 树，然后调用给定的函数，并检查返回值。
+
+*/
 TEST(AvlTreeTest, TestHeight) {
   AVLTree<int> tree;
-  // Test the height of an empty tree
+  // 测试空树的高度
   EXPECT_EQ(tree.CalcHeight(), -1);
-  // Test the height of a tree with one node
+  // 测试只有一个节点的树的高度
   TreeNode<int> *node = new TreeNode<int>(1);
   tree.root_ = node;
   EXPECT_EQ(tree.CalcHeight(), 0);
-  // Test the height of a tree with two nodes
+  // 测试有两个节点的树的高度
   TreeNode<int> *node2 = new TreeNode<int>(2);
   node->rightNode_ = node2;
   EXPECT_EQ(tree.CalcHeight(), 1);
-  // Test the height of a tree with three nodes
+  // 测试有三个节点的树的高度
   TreeNode<int> *node3 = new TreeNode<int>(3);
   node->leftNode_ = node3;
   EXPECT_EQ(tree.CalcHeight(), 1);
 }
 
-// Test the preorder() method of the TreeNode class
+// 测试 TreeNode 类的 preorder() 方法
 TEST(TreeNodeTest, TestPreorder) {
   TreeNode<int> *node = new TreeNode<int>(1);
   preorder(node);
@@ -27,7 +72,7 @@ TEST(TreeNodeTest, TestPreorder) {
   delete node;
 }
 
-// Test the middleorder() method of the TreeNode class
+// 测试 TreeNode 类的 middleorder() 方法
 TEST(TreeNodeTest, TestMiddleorder) {
   TreeNode<int> *node = new TreeNode<int>(1);
   middleorder(node);
@@ -35,7 +80,7 @@ TEST(TreeNodeTest, TestMiddleorder) {
   delete node;
 }
 
-// Test the postorder() method of the TreeNode class
+// 测试 TreeNode 类的 postorder() 方法
 TEST(TreeNodeTest, TestPostorder) {
   TreeNode<int> *node = new TreeNode<int>(1);
   postorder(node);
@@ -43,29 +88,68 @@ TEST(TreeNodeTest, TestPostorder) {
   delete node;
 }
 
-// Test the avlfind() method of the AvlTree class
+// 测试 AvlTree 类的 avlfind() 方法
 TEST(AvlTreeTest, TestAvlfind) {
   AVLTree<int> tree;
-  // Test the case where the value is not found
+  // 测试值未找到的情况
   EXPECT_FALSE(avlfind(5, tree.root_));
-  // Test the case where the value is found
+  // 测试值找到的情况
   TreeNode<int> *node = new TreeNode<int>(3);
   tree.root_ = node;
   EXPECT_TRUE(avlfind(3, tree.root_));
   delete node;
 }
 
-// Test the insertAvl() method of the AvlTree class
+// 测试 AvlTree 类的 insertAvl() 方法
 TEST(AvlTreeTest, TestInsertAvl) {
   AVLTree<int> tree;
-  // Test the case where the tree is empty
+  // 测试树为空的情况
   tree.insert(new TreeNode<int>(3));
   EXPECT_EQ(tree.root_->value_, 3);
-  // Test the case where the tree is not empty
+  // 测试树不为空的情况
   tree.insert(new TreeNode<int>(2));
   EXPECT_EQ(tree.root_->value_, 3);
   EXPECT_TRUE(tree.root_->leftNode_ != nullptr);
   EXPECT_EQ(tree.root_->leftNode_->value_, 2);
   delete tree.root_->leftNode_;
   delete tree.root_;
+}
+
+class AVLTreeTest : public testing::Test {
+protected:
+  void SetUp() override {
+    tree_ = new AVLTree<int>();
+    auto *node = new TreeNode<int>(1);
+    tree_->root_ = node;
+    node->leftNode_ = new TreeNode<int>(2);
+    node->rightNode_ = new TreeNode<int>(3);
+    node->leftNode_->leftNode_ = new TreeNode<int>(4);
+    node->leftNode_->rightNode_ = new TreeNode<int>(5);
+  }
+
+  void TearDown() override { delete tree_; }
+
+  AVLTree<int> *tree_;
+};
+
+/**
+ * @brief 测试函数
+ * @details 用于测试给定的函数是否按预期工作
+ * @param TreeNode<T> *rootNode 要测试的树节点
+ * @param std::function<void(T *)> &func 要执行的函数
+ */
+TEST(AVLTreeTest, FunctionTest) {
+  TreeNode<int> *rootNode = new TreeNode<int>(1);
+  rootNode->leftNode_ = new TreeNode<int>(2);
+  rootNode->rightNode_ = new TreeNode<int>(3);
+  rootNode->leftNode_->leftNode_ = new TreeNode<int>(4);
+  rootNode->leftNode_->rightNode_ = new TreeNode<int>(5);
+
+  std::function<void(int *)> func = [](int *num) {
+    LOG(INFO) << "int for each:" << *num << std::endl;
+  };
+
+  executeEachMid(rootNode, func);
+
+  delete rootNode;
 }
--- src/algorithm/algorithm_avl.h
+++ src/algorithm/algorithm_avl.h
@@ -1,6 +1,51 @@
 #ifndef AVL_TREE_H
 #define AVL_TREE_H
+/*
+这段代码实现了一个AVL树的数据结构，包括树节点的定义和AVL树的操作方法。AVL树是一种平衡二叉搜索树，可以在O(logn)的时间复杂度内进行插入、删除和查找操作。
 
+该代码的核心是AVLTree类和TreeNode结构体。AVLTree类包含了AVL树的插入方法、计算树高度的方法和私有的平衡方法。TreeNode结构体表示树节点，包含了父节点、左子节点、右子节点和值。
+
+AVLTree的插入方法使用递归实现，先找到节点的插入位置，然后进行平衡操作。平衡操作根据节点的左右子树高度差进行判断，如果高度差大于1或小于-1，则进行相应的旋转操作。
+
+除了插入方法，该代码还实现了查找方法avlfind()和遍历方法preorder()、middleorder()、postorder()。其中，avlfind()使用递归在树中查找指定值；preorder()、middleorder()、postorder()分别表示前序遍历、中序遍历和后序遍历。
+
+此外，代码还提供了一个executeEachMid()函数，用于对树的每个节点执行传入的函数。该函数使用中序遍历的方式遍历树，并通过函数指针调用传入的函数。
+总之，这段代码实现了一个AVL树的数据结构，并提供了插入、查找和遍历等方法，可以方便地进行AVL树的操作。
+/////////////////////////////////////////////////
+在AVL树中，旋转操作用于保持树的平衡。当插入或删除节点后，可能会导致树失去平衡，即某个节点的左子树和右子树高度差大于1。为了恢复平衡，需要进行旋转操作。
+
+旋转操作分为左旋和右旋两种。左旋将一个节点的右子树提升为根节点，右子树的左子树成为该节点的右子树。右旋则是左旋的反向操作。
+
+在代码中，左旋操作定义为_rotateLeft()，右旋操作定义为_rotateRight()。下面详细说明它们的实现过程：
+
+左旋操作（_rotateLeft()）：
+
+首先，创建一个临时指针temp指向当前节点的右子节点。
+将当前节点的右子节点指向temp的左子节点。
+如果temp的左子节点不为空，将temp的左子节点的父节点指针指向当前节点。
+将temp的父节点指针指向当前节点的父节点。
+如果当前节点的父节点为空，表示当前节点为根节点，则将根节点指针指向temp。
+如果当前节点是其父节点的左子节点，则将当前节点的父节点的左子节点指针指向temp。
+否则，将当前节点的父节点的右子节点指针指向temp。
+将temp的左子节点指针指向当前节点。
+将当前节点的父节点指针指向temp。
+右旋操作（_rotateRight()）：
+
+首先，创建一个临时指针temp指向当前节点的左子节点。
+将当前节点的左子节点指向temp的右子节点。
+如果temp的右子节点不为空，将temp的右子节点的父节点指针指向当前节点。
+将temp的父节点指针指向当前节点的父节点。
+如果当前节点的父节点为空，表示当前节点为根节点，则将根节点指针指向temp。
+如果当前节点是其父节点的右子节点，则将当前节点的父节点的右子节点指针指向temp。
+否则，将当前节点的父节点的左子节点指针指向temp。
+将temp的右子节点指针指向当前节点。
+将当前节点的父节点指针指向temp。
+这两个旋转操作都会改变树的结构，以保持平衡。旋转操作的触发是在插入节点后进行平衡检查，并根据节点的左右子树高度差来判断需要进行的旋转操作。
+
+在平衡方法balance()中，首先通过_height()方法计算节点的左右子树高度差。如果高度差大于1，则执行左旋或右旋操作，以恢复平衡。具体的旋转操作根据节点的左右子树的高度差来决定，以保证左子树高度大于等于右子树高度。
+*/
+
+#include <functional>
 #include <glog/logging.h>
 
 template <class T> struct TreeNode {
@@ -144,6 +189,17 @@ template <class T> void postorder(TreeNode<T> *rootNode) {
   postorder(rootNode->leftNode_);
   postorder(rootNode->rightNode_);
   LOG(INFO) << rootNode->value_;
+}
+
+template <class T>
+void executeEachMid(TreeNode<T> *rootNode, std::function<void(T *)> &func) {
+
+  if (rootNode == nullptr)
+    return;
+
+  executeEachMid(rootNode->leftNode_, func);
+  func(&(rootNode->value_));
+  executeEachMid(rootNode->rightNode_, func);
 }
 
 #endif /* AVL_TREE_H */
