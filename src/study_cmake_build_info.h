6918e75acd1a1823d7471f06ea8b41e616d19989
singerinsky <singerinsky@gmail.com>
2024-01-25 07:20:02 +0800

    xf

--- src/study_cmake_build_info.h
+++ src/study_cmake_build_info.h
@@ -6,8 +6,8 @@
 struct StudyCmakeBuildInfo
 {
     static inline const std::string GitBranch           = "dev";
-    static inline const std::string CommitSHA           = "f16ce89cc75b797c978079e6016ad716591ba101";
-    static inline const std::string Timestamp           = "2024-01-09T08:39:05";
+    static inline const std::string CommitSHA           = "ddf1367244b223fc20bad45428c31c7acc80d68a";
+    static inline const std::string Timestamp           = "2024-01-25T07:17:28";
     static inline const std::string StudyCmakeVersion   = "";
 };
 
